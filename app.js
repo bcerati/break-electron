416de4395398b4fe50fd85f4e9c34dca71237c57
antoinecqrn <55314585+antoinecqrn@users.noreply.github.com>
2020-01-10 18:38:47 +0100

    Add files via upload
    template accueil (boutons non connectés à BDD) 
    affichage de donnée extraite de fichier JSON

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 window.addEventListener('load', function load(event) {
 
 
@@ -20,113 +17,28 @@ window.addEventListener('load', function load(event) {
   section.appendChild(art);
 
 
+// document.getElementById('login').onclick = function(event) {
+//         var email = document.getElementById('email-input').value;
+//
+//         fetch('http://127.0.0.1:8000/api/login?email=' + email).then(function (data) {
+//           alert(data);
+//         });
+// }
 
 
   var b = document.getElementById('break-button');
     b.onclick = function(event) {
 
-      if (b.value = "Début d'une Pause") {
+      if (b.value === "Début d'une Pause") {
         b.value = "Fin d'une Pause";
         b.style.background = "red";
-      }
-
-      else if (b.value = "Fin d'une Pause") {
-        b.value = "Début d'une Pause";
-        b.style.background = "green";
-    }
-    }
-
-
-
-
-   document.getElementById('btn-bdd').onclick = function(event) {
-      var mysql = require("mysql");
-
-      var connection = mysql.createConnection({
-        host:'devbdd.iutmetz.univ-lorraine.fr',
-        port: '3306',
-        database: 'coqueron3u_breaks',
-        user: 'coqueron3u_appli',
-        password: '31805219'
-
-      })
-
-     // connect to mysql
-     connection.connect(function(err) {
-       // in case of error
-       if(err){
-         console.log(err.code);
-         console.log(err.fatal);
-       } else {
-
-         document.getElementById('btn-bdd').value = "Connecté";
-         document.getElementById('btn-bdd').disable = true;
-       }
-     });
-
-     // Close the connection
-     connection.end(function(){
-       // The connection has been closed
-     });
-
-   }
-
-  function el(selector) {
-    return document.getElementById(selector);
+    } else {
+      b.value = "Début d'une Pause";
+      b.style.background = "green";
   }
 
-  el('action-btn').addEventListener('click', function(){
-    // Get the mysql service
-    getFirstTenRows(function(rows){
-      var html = '';
 
-      rows.forEach(function(row){
-        html += '<tr>';
-        html += '<td>';
-        html += row.ad_mail;
-        html += '</td>';
-        html += '<td>';
-        html += row.id_utilisateur;
-        html += '</td>';
-        html += '</tr>';
-        console.log(row);
-      });
-
-      document.querySelector('#table > tbody').innerHTML = html;
-    });
-  },false);
-
-  function getFirstTenRows(callback){
-    var mysql = require("mysql");
-    var connection = mysql.createConnection({
-      host:'devbdd.iutmetz.univ-lorraine.fr',
-      port: '3306',
-      database: 'coqueron3u_breaks',
-      user: 'coqueron3u_appli',
-      password: '31805219'
-
-    })
-
-    // Perform a query
-    $query = 'SELECT id_utilisateur, ad_mail FROM `utilisateur` LIMIT 10';
-
-    connection.query($query, function(err, rows, fields) {
-      if(err){
-        console.log("An error ocurred performing the query.");
-        console.log(err);
-        return;
-      }
-
-      callback(rows);
-
-      console.log("Query succesfully executed");
-    });
-
-    // Close the connection
-    connection.end(function(){
-      // The connection has been closed
-    });
-  }
+}
 
 
 
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const electron = require('electron')
 
 const { app, BrowserWindow } = require('electron')
+var path = require('path')
 
 function createWindow () {
   // Cree la fenetre du navigateur.
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+  //  icon:path.join(__dirname + ‘/clock.icns’),
     webPreferences: {
       nodeIntegration: true
     }
